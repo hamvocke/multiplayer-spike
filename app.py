f54ed9baecda175829bd97e7369bf86ccc914d7f
Ham Vocke <ham@hamvocke.com>
2020-12-23 10:31:07 +0100

    handle socket connections, parse messages

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sockets import Sockets
 import json
 
 app = Flask(__name__)
+app.debug = True
 sockets = Sockets(app)
 
 all_games = {}
@@ -45,17 +46,27 @@ def game(game_slug):
     return render_template('waiting_room.html', game_slug=g["slug"], players=g["players"])
 
 
-@sockets.route('/join')
-def join_socket(ws):
+@sockets.route('/game/socket')
+def game_socket(ws):
     while not ws.closed:
         message = ws.receive()
 
         payload = json.loads(message)
+        print("received message " + message)
 
-        if payload is not None and payload["type"] == "game":
-            print("received a game message")
-        
-        ws.send(json.dumps(game))
+
+        if payload["type"] == "join":
+            player = payload["name"]
+            game_slug = payload["game_slug"]
+            print(f"{player} joined {game_slug}")
+
+            g = all_games.get(game_slug, None)
+
+            if g is None:
+                ws.send(json.dumps({"status": "error", "message": f"game {game_slug} does not exist"}))
+            else:
+                for client in ws.handler.server.clients.values():
+                    client.ws.send(json.dumps({"status": "success", "message": g}))
 
 
 if __name__ == "__main__":
